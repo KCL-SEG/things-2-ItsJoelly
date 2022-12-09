149848eac97b7da6e5865fb2a6683d85919561c0
Joel Jolly <joel.jolly@kcl.ac.uk>
2022-12-09 15:28:57 +0000

    edit all

--- things/forms.py
+++ things/forms.py
@@ -1,17 +1,12 @@
 """Forms of the project."""
 
 from django import forms
-from django.core.validators import MinValueValidator, MaxValueValidator
 from .models import Thing
 # Create your forms here.
 
 class ThingForm(forms.ModelForm):
-    # name = forms.CharField(label="Name",max_length=35)
-
-    # quantity = forms.IntegerField(label ="Quantity",validators=[MinValueValidator(0),MaxValueValidator(50)])
-
-
     class Meta:
         model = Thing
-        fields = ['name', 'description', 'quantity']
-    widgets = {"description": forms.Textarea(), "quantity": forms.NumberInput()}
+        
+        fields = ["name", "description", "quantity"]
+        widgets = {"description": forms.Textarea(), "quantity": forms.NumberInput()}
--- things/views.py
+++ things/views.py
@@ -2,10 +2,5 @@ from django.shortcuts import render
 from .forms import ThingForm
 
 def home(request):
-    form = ThingForm()
-    if request.method == 'POST':
-        form = ThingForm(request.POST)
-        if form.is_valid():
-            form.save()
-            
+    form = ThingForm()        
     return render(request, 'home.html', {'form': form})
